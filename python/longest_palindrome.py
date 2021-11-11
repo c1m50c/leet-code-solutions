05df1cac2b253266c73abcb3795cd0b5e6d4b036
c1m50c <terminalityms@gmail.com>
2021-11-10 23:37:24 -0600

    Longest Palindrome 🐍🧵

---
+++ python/longest_palindrome.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/longest-palindromic-substring/ #
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n: int = len(s)
+        result: list[str] = [  ]
+        if n <= 1: return s
+
+        for i in range(0, n):
+            l, r, = i, i # l: Left, r: Right
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r - l + 1 > len(result):
+                    result = s[l : r + 1]
+                l -= 1
+                r += 1
+
+            l, r = i - 1, i
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r - l + 1 > len(result):
+                    result = s[l : r + 1]
+                l -= 1
+                r += 1
+
+        return result
