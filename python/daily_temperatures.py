5385c5255b51dd529bfbceb67b17b70f4b952cc3
c1m50c <terminalityms@gmail.com>
2021-11-13 16:11:05 -0600

    Daily Temperatures 📅🔥

---
+++ python/daily_temperatures.py
@@ -0,0 +1,23 @@
+from typing import *
+
+
+class Solution:
+    @staticmethod
+    def dailyTemperatures(temperatures: List[int]) -> List[int]:
+        n: int = len(temperatures)
+        result: List[int] = [0] * n
+        h: int = 0
+        
+        for i in range(n - 1, -1, -1):
+            current: int = temperatures[i]
+            
+            if current >= h:
+                h = current
+                continue
+            
+            gap: int = 1
+            while temperatures[i + gap] <= current:
+                gap += result[i + gap]
+            result[i] = gap
+        
+        return result
